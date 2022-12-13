e7d6a6c1fa97336f784424f887ca85b7dd6e1b67
Arnau Urtós <arnauurtos@inspladelestany.cat>
2022-12-13 20:06:11 +0200

    Footer java script

---
+++ footer.js
@@ -0,0 +1,23 @@
+
+var header = document.getElementById("footer");
+header.innerHTML = `
+        <div class="container">
+            <p id="creators">
+                Els Creadors
+            </p>
+            <p id="names">
+                Pau España
+                <br>
+                Arnau Urtós
+                <br>
+                Sergi Vilagran
+                <br>
+                Alberto Revert
+            </p>
+        </div>
+        <div class="container">
+            <p id="sponsors">
+                Sponsors
+            </p>
+        </div>
+`;
